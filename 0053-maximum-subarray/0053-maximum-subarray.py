2970fc0a7b647968c9758e2ab497ec1f0366d296
Rudra Patel <rudrapatel2156@gmail.com>
2026-07-22 19:09:22 +0530

    Time: 28 ms (78.94%), Space: 31.3 MB (94.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,12 +3,8 @@ class Solution:
         current_sum = nums[0]
         max_sum = nums[0]
 
-# [-2,1,-3,4,-1,2,1,-5,4]
         for i in range(1, len(nums)):
-            # Decide:
-            # Continue previous subarray: current_sum + nums[i]
-            # OR
-            # Start fresh: nums[i]  
+
             if current_sum + nums[i] <  nums[i]:
                 current_sum = nums[i]
             else :
@@ -18,4 +14,3 @@ class Solution:
                 max_sum = current_sum
 
         return max_sum
-            # Then update max_sum
