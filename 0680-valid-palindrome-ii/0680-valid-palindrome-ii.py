2fd9fa8bdca20f4262ba51e2c738056d59560c9a
Rudra Patel <rudrapatel2156@gmail.com>
2026-08-18 14:04:48 +0530

    Time: 58 ms (53.12%), Space: 19.5 MB (80.26%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        def check(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+        
+        while left < right:
+
+            if s[left] != s[right]:
+                return   check(left+1, right) or check(left, right-1)
+
+            left += 1
+            right -= 1
+            
+        return True
